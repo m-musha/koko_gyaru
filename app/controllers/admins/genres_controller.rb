751e3b739a4a6f613c66e62de61d6cb33207b7f2
m-musha <gnmknr@gmail.com>
2021-05-08 08:15:38 +0000

    [ADD] ジャンルコントローラ

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,2 +1,30 @@
 class Admins::GenresController < ApplicationController
+  
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to admins_genres_path
+  end
+  
+  def index
+    @genre = Genre.new
+    @genres = Genres.all
+  end
+  
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admins_genres_path
+  end 
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+    
+
 end
