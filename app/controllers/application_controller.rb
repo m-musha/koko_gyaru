20bbc898cd0a73f3752e0287913f6b7343f18e7e
m-musha <gnmknr@gmail.com>
2021-05-25 23:39:01 +0000

    ログイン後のpath変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,22 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+        admins_genres_path
+    else
+        words_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :member
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+  end
+
 end
